12a6901d438c6ceecab02b9579654cbf95486d67
David Donayo <44318928+daviddonayo@users.noreply.github.com>
2020-07-31 00:39:36 -0300

    30/7/20 login, menu, falta documentacion

--- target/tibuuroncitos-1.0-SNAPSHOT/code.js
+++ target/tibuuroncitos-1.0-SNAPSHOT/code.js
@@ -3,14 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 $(document).ready(function(){
+    var ID = sessionStorage.getItem('idCliente'); 
     $.ajax({
-        url: 'http://localhost:8080/tibuuroncitos/rest/cliente'
+        url: 'http://localhost:8080/tibuuroncitos/rest/cliente/' + ID
     }).done(function(data){
-        $.each(data, function(i, item) {
-            $("#clientes").append('<li style="padding:0.1rem;">'+ '<button type="button" class="btn btn-info" onclick="datosCliente('+item['id']+')">Ver</button>'+ '&nbsp;&nbsp;&nbsp;' +item['nombre']+'</li>');
-        });
+        $("#id").text(data['id']);
+        $("#nombre").text(data['nombre']);
+        $("#apellido").text(data['apellido']);
+        $("#dni").text(data['DNI']);
+        $("#fechaNacimiento").text(data['fechaNacimiento']);
+        $("#situacionEco").text(data['situacionEco']);
+        $("#telefono").text(data['telefono']);
+        $("#direccion").text(data['direccion']);
     });
 });
 
@@ -62,7 +67,7 @@ $(document).ready(function(){
             error: function (data) {
                 console.log('Error:', data);
             }
-        })
+        });
     });
     $("#eliminarBono").click(function(x){
         x.preventDefault();
@@ -78,7 +83,7 @@ $(document).ready(function(){
             error: function (data) {
                 console.log('Error:', data);
             }
-        })
+        });
     });
 });
 //JS Cuentas api
@@ -99,7 +104,7 @@ $(document).ready(function(){
             error: function (data) {
                 console.log('Error:', data);
             }
-        })
+        });
     });
     $("#eliminarCuenta").click(function(x){
         x.preventDefault();
@@ -118,94 +123,18 @@ $(document).ready(function(){
         });
     });
 });
-//JS Clientes api
-$(document).ready(function(){
-    $("#enviarCliente").click(function(x){
-        x.preventDefault();
-        let nombre = $("#inputNombreCliente").val();
-        let apellido = $("#inputApellidoCliente").val();
-        let dni = $("#inputDNICliente").val();
-        let fechaNacimiento = $("#inputDateCliente").val() + "T03:00:00Z[UTC]";
-        let situacionEco = $("#inputSituacionEcoCliente").val();
-        let telefono = $("#inputTelefonoCliente").val();
-        let direccion = $("#inputDireccionCliente").val();
-        console.log(nombre);
-        console.log(apellido);
-        console.log(dni);
-        console.log(fechaNacimiento);
-        console.log(situacionEco);
-        console.log(telefono);
-        console.log(direccion);
-         $.ajax({
-            url: "http://localhost:8080/tibuuroncitos/rest/cliente",
-            type: 'post',
-            contentType: 'application/json',
-            dataType: 'json',
-            //data: JSON.stringify({"nombre": nombre, "apellido": apellido, "DNI": dni, "fechaNacimiento": fechaNacimiento, "situacionEco": situacionEco, "telefono": telefono, "direccion": direccion}),
-            data: JSON.stringify({
-                                    "apellido": nombre,
-                                    "DNI": dni,
-                                    "direccion": direccion,
-                                    "fechaNacimiento": fechaNacimiento,
-                                    "nombre": nombre,
-                                    "situacionEco": situacionEco,
-                                    "telefono": telefono
-                                }),
-            success: function (){
-                window.location.reload(true);
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
-    });
-    $("#eliminarCliente").click(function(x){
-        x.preventDefault();
-        let nroCliente = $("#inputNroCliente").val();
-        $.ajax({
-            url: "http://localhost:8080/tibuuroncitos/rest/cliente/" + nroCliente,
-            type: 'delete',
-            contentType: 'application/json',
-            dataType: 'json',
-            success: function (){
-                window.location.reload(true);
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        })
-    });
-});
 //JS Transacciones api
-var estadoTrans;
 $(document).ready(function(){
     $("#enviarDeposito").click(function(x){
         x.preventDefault();   
         let current_datetime = new Date();
         let formatted_date = current_datetime.getFullYear() + "-" + appendLeadingZeroes(current_datetime.getMonth() + 1) + "-" + appendLeadingZeroes(current_datetime.getDate()) + "T03:00:00Z[UTC]";
-        let bandera = 0;
+        
         let nroCuentaDeposito = $("#inputNroCuentaDeposito").val();
-        estadoTrans = "En Proceso";
-        $.ajax({
-            url:"http://localhost:8080/tibuuroncitos/rest/cuenta",
-            type: 'get'
-        }).done(function(data){
-            $.each(data, function(item) {
-                if(item['nroCuenta'] === nroCuentaDeposito){
-                    bandera = 1;
-                }
-            }); 
-            if(bandera === 0){
-                alert('El número de cuenta que utilizaste no es válido.');
-                estadoTrans = "Cancelada";
-                console.log(estadoTrans);
-            }
-        });
-        console.log(bandera);
-        console.log(estadoTrans);
         let montoDeposito = $("#inputMontoDeposito").val();
         let fechaDeposito = formatted_date;
         let tipoTrans = "Depósito";
+        let estadoTrans = "En Proceso";
         $.ajax({
             url: "http://localhost:8080/tibuuroncitos/rest/transaccion",
             type: 'post',
@@ -224,14 +153,12 @@ $(document).ready(function(){
                 console.log('Error:', data);
             }
         });
+        
         $.ajax({
             url:"http://localhost:8080/tibuuroncitos/rest/cuenta/" + nroCuentaDeposito,
             type: 'get'
         }).done(function(data){
-            if(typeof data === 'undefined' || data === null || data['nroCuenta'] !== 'Habilitada'){
-                alert('El número de cuenta que utilizaste no es valido.');
-            }
-            else {
+            if (typeof data !== 'undefined' && data !== null && data['estadoCuenta'] === 'Habilitada'){
                 $.ajax({
                     url: "http://localhost:8080/tibuuroncitos/rest/cuenta/" + nroCuentaDeposito,
                     type: 'put',
@@ -249,6 +176,10 @@ $(document).ready(function(){
                         console.log('Error:', data);
                     }  
                 });
+            }else{
+                alert('La cuenta a la que usted quiere depositar, no esta disponible.');
+                window.location.reload(true);
+                actualizarTransaccion();
             }
 
         });
@@ -268,30 +199,15 @@ $(document).ready(function(){
             error: function (data) {
                 console.log('Error:', data);
             }
-        })
+        });
     });
 });
-
-function datosCliente(idCliente){
-    $.ajax({
-        url: 'http://localhost:8080/tibuuroncitos/rest/cliente/'+idCliente
-    }).done(function(data){
-        $("#id").text(data['id']);
-        $("#nombre").text(data['nombre']);
-        $("#apellido").text(data['apellido']);
-        $("#dni").text(data['DNI']);
-        $("#fechaNacimiento").text(data['fechaNacimiento']);
-        $("#situacionEco").text(data['situacionEco']);
-        $("#telefono").text(data['telefono']);
-        $("#direccion").text(data['direccion']);
-    });
-}
 
 function appendLeadingZeroes(n){
   if(n <= 9){
     return "0" + n;
   }
-  return n
+  return n;
 }
 
 function datosTransaccion(nroTrans){
